dca23e9886d0ffa9139e7bd4089a2d0d795059d7
NANDbrew <zortman@gmail.com>
2024-07-20 10:17:38 -0500

    Fix aestrin compass inventory rotation. Fix tobacco box inventory rotation

---
+++ Patches/AestrinCompassPatches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(ShipItemCompass))]
+    internal static class AestrinCompassPatches
+    {
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        public static void AwakePatch(ShipItemCompass __instance)
+        {
+            if (__instance.GetPrefabIndex() == 82)
+            {
+                __instance.inventoryRotation = 180f;
+                __instance.inventoryRotationX = 270f;
+            }
+        }
+    }
+}
+
---
+++ Patches/TobaccoBoxPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NANDFixes.Patches
+{
+    internal class TobaccoBoxPatches
+    {
+        [HarmonyPatch(typeof(ShipItemCrate))]
+        private static class BarrelDrinkPatches
+        {
+            [HarmonyPatch("Awake")]
+            [HarmonyPostfix]
+            public static void ExtraLateUpdatePatch(ShipItemCrate __instance, ref float __heldRotationOffset)
+            {
+                if (__instance.GetPrefabIndex() >= 311 && __instance.GetPrefabIndex() <= 319)
+                {
+                    __heldRotationOffset = 45f;
+                    __instance.inventoryRotation = 180f;
+                    __instance.inventoryRotationX = 270f;
+                }
+            }
+        }
+    }
+}
