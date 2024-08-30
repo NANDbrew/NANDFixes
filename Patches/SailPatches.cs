5219d10d6e8b54fd6b2e5a073f11e3e1eed7db69
NANDbrew <zortman@gmail.com>
2024-08-30 13:40:22 -0500

    fix for junk gaff collision issue

---
+++ Patches/SailPatches.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(ShipyardSailColChecker), "Awake")]
+    internal class SailPatches
+    {
+        public static void Postfix(Sail ___sail, ref Vector3 ___sailModelOffset)
+        {
+            if (colfixSails.Contains(___sail.prefabIndex))
+            {
+                ___sailModelOffset = new Vector3(0f, 0f, 1.5f);
+            }
+        }
+
+        private static readonly int[] colfixSails = new int[] { 17, 18, 107 };
+    }
+}
