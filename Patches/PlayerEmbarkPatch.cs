622161e190d4856f9dd46e13dc1777041c653010
NANDbrew <zortman@gmail.com>
2024-10-23 23:35:55 -0500

    New fix for player falling through boat when boarding from another boat

---
+++ Patches/PlayerEmbarkPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "Update")]
+    internal static class PlayerEmbarkPatch
+    {
+        public static void Postfix(PlayerEmbarkDisembarkTrigger __instance, ref bool ___exitBoatFlag, Collider ___currentlyStayedTrigger, ref Collider ___currentBoatCollider, ref float ___disembarkHeight, Transform ___playerObserver, bool __runOriginal)
+        {
+            if (!Plugin.playerEmbark.Value || !__runOriginal) return;
+            if (PlayerEmbarkDisembarkTrigger.embarked && !GameState.sleeping && !GameState.justWokeUp && !GameState.currentShipyard)
+            {
+                if (___currentlyStayedTrigger != null && ___currentlyStayedTrigger != ___currentBoatCollider && !(bool)AccessTools.Method(__instance.GetType(), "IsGroundedOnBoat").Invoke(__instance, null))
+                {
+                    ___exitBoatFlag = true;
+                    ___disembarkHeight = ___playerObserver.position.y;
+                }
+            }
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -12,21 +12,23 @@ namespace NANDFixes
     {
         public const string PLUGIN_ID = "com.nandbrew.nandfixes";
         public const string PLUGIN_NAME = "NAND Fixes";
-        public const string PLUGIN_VERSION = "1.0.3";
+        public const string PLUGIN_VERSION = "1.0.4";
 
         //--settings--
-        internal static ConfigEntry<bool> hook_shelf;
+        //internal static ConfigEntry<bool> hook_shelf;
         internal static ConfigEntry<bool> stickyFix;
         internal static ConfigEntry<bool> bedCamAdjust;
-
+        internal static ConfigEntry<bool> playerEmbark;
 
         private void Awake()
         {
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
 
-            hook_shelf = Config.Bind("StickyFix", "Hook & Shelf fixer", true, new ConfigDescription("Keep lantern hooks and shelves stuck to boat. If they're causing issues, turn this off (does nothing if StickyFix is off)"));
+            //hook_shelf = Config.Bind("StickyFix", "Hook & Shelf fixer (obsolete)", false, new ConfigDescription("Keep lantern hooks and shelves stuck to boat. If they're causing issues, turn this off (does nothing if StickyFix is off)"));
             stickyFix = Config.Bind("StickyFix", "StickyFix", true, new ConfigDescription("Fix the infamous things-getting-stuck-to-other-boats bug"));
             bedCamAdjust = Config.Bind("misc.", "Bed camera adjustment", true, new ConfigDescription("Moves the sleep position in certain beds up a bit to fix the camera clipping through"));
+            playerEmbark = Config.Bind("Embark", "Boat-to-boat embark fix", true, new ConfigDescription("Fix for the \"falling through the deck when jumping between boats\" issue"));
+
         }
     }
 }
