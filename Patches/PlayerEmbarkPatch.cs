09d2b48a91d40daa641e65648208871186cda2a1
NANDbrew <zortman@gmail.com>
2025-02-04 20:22:22 -0600

    Improved player embark patch to use a list like the ship item embark patch

--- Patches/PlayerEmbarkPatch.cs
+++ Patches/PlayerEmbarkPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using NANDFixes.Scripts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,58 @@ using UnityEngine;
 
 namespace NANDFixes.Patches
 {
-    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "Update")]
+    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger))]
     internal static class PlayerEmbarkPatch
     {
-        public static void Postfix(PlayerEmbarkDisembarkTrigger __instance, ref bool ___exitBoatFlag, Collider ___currentlyStayedTrigger, ref Collider ___currentBoatCollider, ref float ___disembarkHeight, Transform ___playerObserver, bool __runOriginal)
+        [HarmonyPatch("Awake")]
+        [HarmonyPrefix]
+        public static void Awake(PlayerEmbarkDisembarkTrigger __instance)
+        {
+            __instance.gameObject.AddComponent<Scripts.EmbarkTracker>();
+        }
+
+        [HarmonyPatch("OnTriggerEnter")]
+        [HarmonyPrefix]
+        public static bool OnTriggerEnter(Collider other, PlayerEmbarkDisembarkTrigger __instance, ref BoatEmbarkCollider ___currentlyStayedEmbarkCol, ref bool ___exitBoatFlag, ref Collider ___currentlyStayedTrigger)
+        {
+            if (!Plugin.playerEmbark.Value) return true;
+
+            if (other.CompareTag("EmbarkCol"))
+            {
+                List<Collider> colliders = __instance.GetComponent<Scripts.EmbarkTracker>().embarkColliders;
+                if (!colliders.Contains(other))
+                {
+                    colliders.Add(other);
+                }
+                if (colliders.Count > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        [HarmonyPatch("OnTriggerExit")]
+        [HarmonyPostfix]
+        public static void OnTriggerExit(PlayerEmbarkDisembarkTrigger __instance, Collider other, ref BoatEmbarkCollider ___currentlyStayedEmbarkCol, ref bool ___exitBoatFlag, ref Collider ___currentlyStayedTrigger)
+        {
+            if (!Plugin.playerEmbark.Value) return;
+
+            List<Collider> colliders = __instance.GetComponent<Scripts.EmbarkTracker>().embarkColliders;
+            colliders.Remove(other);
+
+            if (colliders.Count >= 1)
+            {
+                ___currentlyStayedTrigger = colliders.Last();
+                ___currentlyStayedEmbarkCol = ___currentlyStayedTrigger.GetComponent<BoatEmbarkCollider>();
+                ___currentlyStayedEmbarkCol.ToggleBoatCapsuleCol(newState: false);
+                ___exitBoatFlag = false;
+
+            }
+        }
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void UpdatePatch(PlayerEmbarkDisembarkTrigger __instance, ref bool ___exitBoatFlag, Collider ___currentlyStayedTrigger, ref Collider ___currentBoatCollider, ref float ___disembarkHeight, Transform ___playerObserver, bool __runOriginal)
         {
             if (!Plugin.playerEmbark.Value || !__runOriginal) return;
             if (PlayerEmbarkDisembarkTrigger.embarked && !GameState.sleeping && !GameState.justWokeUp && !GameState.currentShipyard)
