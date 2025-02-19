a1ce143862d7c53217d1b35d1776b77e69e35827
NANDbrew <zortman@gmail.com>
2025-02-19 02:13:59 -0600

    Add fix for floating "sell item" scroll

---
+++ Patches/BuyItemUIPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(BuyItemUI), "Update")]
+    internal class BuyItemUIPatch
+    {
+        public static void Postfix(BuyItemUI __instance)
+        {
+            if (__instance.menu.activeInHierarchy && (!__instance.activeItem || __instance.activeItem && !__instance.activeItem.held))
+            {
+                __instance.DeactivateUI();
+            }
+        }
+    }
+}
