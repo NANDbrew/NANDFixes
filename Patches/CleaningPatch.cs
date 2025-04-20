eb48d9630f9bd6a7f2be3c52b1932015a3d2318a
NANDbrew <zortman@gmail.com>
2025-04-20 12:40:04 -0500

    add patch for Jong cleaning crash.

---
+++ Patches/CleaningPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NANDFixes
+{
+    [HarmonyPatch(typeof(Shipyard), "CleanHull")]
+    internal static class CleaningPatch
+    {
+        public static bool Prefix(/*Shipyard __instance, */GameObject ___currentShip, ref bool ___currentOrderIncludesCleaning)
+        {
+            if (___currentShip.GetComponent<CleanableObject>() == null)
+            {
+                NotificationUi.instance.ShowNotification("Ship is not cleanable");
+                ___currentOrderIncludesCleaning = false;
+                //__instance.UpdateOrder();
+                return false;
+            }
+            return true;
+        }
+    }
+}
