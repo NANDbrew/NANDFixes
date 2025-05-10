007967d2362cafe8b9523578ba5687c083a87e9e
NANDbrew <zortman@gmail.com>
2025-05-10 16:13:49 -0500

    fixed cleaning patch breaking

--- Patches/CleaningPatch.cs
+++ Patches/CleaningPatch.cs
@@ -11,13 +11,12 @@ namespace NANDFixes
     [HarmonyPatch(typeof(Shipyard), "CleanHull")]
     internal static class CleaningPatch
     {
-        public static bool Prefix(/*Shipyard __instance, */GameObject ___currentShip, ref bool ___currentOrderIncludesCleaning)
+        public static bool Prefix(GameObject ___currentShip, ref bool ___currentOrderIncludesCleaning)
         {
-            if (___currentShip.GetComponent<CleanableObject>() == null)
+            if (___currentShip.GetComponentInChildren<CleanableObject>() == null)
             {
                 NotificationUi.instance.ShowNotification("Ship is not cleanable");
                 ___currentOrderIncludesCleaning = false;
-                //__instance.UpdateOrder();
                 return false;
             }
             return true;
