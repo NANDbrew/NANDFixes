d0db6f406a8beda0d76195ce4314fc2a4178ce29
NANDbrew <zortman@gmail.com>
2025-07-22 19:03:19 -0500

    new patch "MushroomFix" fixes cave/field mushrooms falling through the floor

---
+++ Patches/MushroomFix.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(ShipItemFood), "OnLoad")]
+    public static class MushroomPatch
+    {
+        public static void Postfix(ShipItemFood __instance)
+        {
+            if (!Plugin.mushroomFix.Value) return;
+            int index = __instance.GetPrefabIndex();
+            if (index == 144 || index == 145)
+            {
+                var oldCol = __instance.gameObject.GetComponent<SphereCollider>();
+                if (oldCol == null)
+                {
+                    return;
+                }
+                oldCol.enabled = false;
+                var col = __instance.gameObject.AddComponent<CapsuleCollider>();
+                col.isTrigger = true;
+                col.height = 0f;
+                col.radius = oldCol.radius;
+                col.center = oldCol.center;
+                Component.Destroy(oldCol);
+            }
+        }
+    }
+}
