615ca6384ebc9f9b4dbc84bd50d684b350012d2d
NANDbrew <zortman@gmail.com>
2025-08-25 20:43:59 -0500

    bump version number

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ namespace NANDFixes
     {
         public const string PLUGIN_ID = "com.nandbrew.nandfixes";
         public const string PLUGIN_NAME = "NAND Fixes";
-        public const string PLUGIN_VERSION = "1.2.5";
+        public const string PLUGIN_VERSION = "1.2.6";
 
         //--settings--
         //internal static ConfigEntry<bool> hook_shelf;
