94f1fa6f22544b0f78923ac017250e868e478e8b
NANDbrew <zortman@gmail.com>
2025-10-25 11:00:37 -0500

    new patch: Demand Fix. fixes salt, rubber, coffee, and saffron breaking missions

---
+++ Patches/DemandFix.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(IslandEconomy))]
+    internal static class DemandFix
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        public static void Patch1(ref int[] ___baseDemand)
+        {
+            if (!Plugin.demandFix.Value) return;
+            if (___baseDemand.Length < 65)
+            {
+                Array.Resize(ref ___baseDemand, 65);
+            }
+            Debug.Log("NANDFixes: resized demand array");
+        }
+
+    }
+}
