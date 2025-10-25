38b5ec9ce3d67d526958b7dc0ecbd323856353b1
NANDbrew <zortman@gmail.com>
2025-10-25 11:03:58 -0500

    new patch: Mission Penalty Fix. fixes reported reputation penalty in the "abandoning mission" notification

---
+++ Patches/MissionPenaltyUIPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(PlayerMissions), "AbandonMission")]
+    internal static class MissionPenaltyUIPatch
+    {
+        public static void Prefix(int missionIndex, ref string __state)
+        {
+            if (!Plugin.missionPenaltyFix.Value) return;
+            string region = regionNames[PlayerMissions.missions[missionIndex].originPort.region] ?? "";
+            var num = (PlayerMissions.missions[missionIndex].goodCount - PlayerMissions.missions[missionIndex].GetDeliveredCount()) * 100;
+            __state = "Abandoning mission:\n" + PlayerMissions.missions[missionIndex].missionName + "\n-" + num.ToString() + " " + region + " reputation";
+        }
+        public static void Postfix(string __state)
+        {
+            if (!Plugin.missionPenaltyFix.Value) return;
+            if (__state != null && __state.Length > 0)
+            {
+                NotificationUi.instance.ShowNotification(__state);
+            }
+        }
+        static readonly Dictionary<PortRegion, string> regionNames = new Dictionary<PortRegion, string>()
+        {
+            { PortRegion.alankh, "Al\'Ankh" },
+            { PortRegion.emerald, "Emerald" },
+            { PortRegion.medi, "Aestrin" },
+            { PortRegion.none, "" },
+        };
+    }
+}
