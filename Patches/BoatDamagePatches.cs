f9b40642991cbd08f721ab64967b3f37a2f9f7dd
NANDbrew <zortman@gmail.com>
2025-10-25 10:55:48 -0500

    added patch to fix boat damage overlay rendering over water

---
+++ Patches/BoatDamagePatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(BoatDamageWater), "Start")]
+    internal static class BoatDamagePatch
+    {
+        public static void Postfix(Renderer ___renderer)
+        {
+            if (!Plugin.damagePatch.Value) return;
+            ___renderer.sharedMaterial.renderQueue = 2002;
+        }
+    }
+}
