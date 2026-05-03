a422f9a352429023672c411f295640b3c18d029f
NANDbrew <zortman@gmail.com>
2026-05-03 09:59:39 -0500

    add patch to fix coconut crate saying "coconuts" twice

---
+++ Patches/CoconutTextPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(PrefabsDirectory), "PopulateShipItems")]
+    internal static class CoconutTextPatch
+    {
+        public static void Postfix()
+        {
+            PrefabsDirectory.instance.shipItems[3].description = "";
+        }
+    }
+}
