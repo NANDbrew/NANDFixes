76d7854f3736fdfd299e48ede3e5210be58ef66a
NANDbrew <zortman@gmail.com>
2026-08-04 18:21:43 -0500

    new patch fixes inventory hotkeys trying to do stuff while game is paused

---
+++ Patches/HotkeyFix.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NANDFixes.Patches
+{
+    [HarmonyPatch(typeof(InventoryHotkeys), "Update")]
+    internal static class HotkeyPatches
+    {
+        internal static bool Prefix()
+        {
+            //if (!Plugin.spinFix.Value) return true;
+            if (GameState.wasInSettingsMenu)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
